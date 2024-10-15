ae8584ca05555185f3b669b086b88fa70eefd1a9
Diprati Das <102643011+developer0311@users.noreply.github.com>
2024-10-16 01:14:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -533,7 +533,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/home",
+      callbackURL: "https://a-pen-s-domain.onrender.com/auth/google/home",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
