ff1f8553765d831b6c83df13d905ae9eac936a56
Diprati Das <dipratidas2004@gmail.com>
2024-10-16 22:28:22 +0530

    before final 2

--- index.js
+++ index.js
@@ -550,8 +550,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // callbackURL: "https://a-pen-s-domain.onrender.com/auth/google/home",
-      callbackURL: "http://localhost:3000/auth/google/home",
+      callbackURL: "https://a-pen-s-domain.onrender.com/auth/google/home",
+      // callbackURL: "http://localhost:3000/auth/google/home",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
